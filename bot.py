ded214be49407057a49af709a54ecc698dbb6f08
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2019-03-23 02:38:01 +0800

    Expanded list of statements
    Included 3 statements for data to be recorded:message, emotion, intensity. Added reply. Config file created but gitignored.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import config
 
 global count
-count = 1
+count = 4
 updater = Updater(token=config.Token, use_context=True)
 dispatcher = updater.dispatcher
 
@@ -19,10 +19,32 @@ def start(update, context):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+responsetext1 = """Thank you for your input. Among the 8 emotions below, which response would you expect? Please key in a number. Alternatively, you may use /list command to find out the full list of emotions.
+1. Joy
+2. Sadness
+3. Trust
+4. Disgust
+5. Fear
+6. Anger
+7. Surprise
+8. Anticipation
+"""
+
+responsetext2 = """From a scale of 1-3, 3 being the most severe, how would you rate the intensity of the emotion?"""
+
+responsetext3 = """Thank you for your input!""" 
+
 def trigger(update, context):
     global count
-    context.bot.send_message(chat_id=update.message.chat_id, text="哈咯小鱼尾")
-    sheet.update_cell(count, 1, update.message.text)
+    if (count%3==1):
+        context.bot.send_message(chat_id=update.message.chat_id, text=responsetext1)
+        sheet.update_cell((count//3)+1, 1, update.message.text)
+    elif (count%3==2):
+        context.bot.send_message(chat_id=update.message.chat_id, text=responsetext2)
+        sheet.update_cell((count//3)+1, 2, update.message.text)
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text=responsetext3)
+        sheet.update_cell((count//3), 3, update.message.text)
     count+=1
 
 trigger_handler = MessageHandler(Filters.all,trigger)
